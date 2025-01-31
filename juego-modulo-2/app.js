44d8dbfc2d2e8da58ba25f85ed01de2634e8aa20
Omar Lengua <omarns21@gmail.com>
2025-01-31 17:40:16 +0100

    Changes: def another dir, def second project and set the files into directory

---
+++ juego-modulo-2/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del nùmero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un nùmero del 1 al 10';
