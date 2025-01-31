96c8fbe01299d3f57674f72a2b21f63a4a92a1c2
Omar Lengua <omarns21@gmail.com>
2025-01-31 16:51:11 +0100

    Implement: project find secret number, completed, awaiting to do the second module

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroMaximoPosible = 10;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximoPosible) + 1;
+let numeroUsuario = 0;
+let intentos = 1;
+let maximoIntento = 3;
 
-console.log(numeroUsuario);
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Indícame un número del 1 al ${numeroMaximoPosible}, por favor`));
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        intentos++;
+        if (intentos > maximoIntento) {
+            alert(`Llegaste al número máximo de ${maximoIntento} intentos.`);
+            break;
+        }
+    }
 }
